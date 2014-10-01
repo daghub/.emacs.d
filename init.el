8990d0c57d74b510aeb1a561cd2895d7c809b0b7
Dag Ekengren <daekengr@microsoft.com>
2014-10-01 23:19:58 +0200

    moves ggtags

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/git-modes")
 (add-to-list 'load-path "~/.emacs.d/lisp/auto-complete-etags")
 (add-to-list 'load-path "~/.emacs.d/lisp/plantuml-mode")
-(add-to-list 'load-path "~/.emacs.d/ggtags")
+(add-to-list 'load-path "~/.emacs.d/lisp/ggtags")
 (add-to-list 'load-path "~/.emacs.d/neotree")
 
 ;; Treat .h files at c++ headers
