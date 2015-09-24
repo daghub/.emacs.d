38538f88e925b4ef9d46424430e515fd1ecb5f30
Dag Ekengren <daekengr@microsoft.com>
2015-09-24 14:16:59 +0200

    adds god mode

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/bm")
 (add-to-list 'load-path "~/.emacs.d/lisp/iedit")
 (add-to-list 'load-path "~/.emacs.d/lisp/dtrt-indent")
+(add-to-list 'load-path "~/.emacs.d/god-mode")
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; Color theme
@@ -44,6 +45,28 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; god-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'god-mode)
+(god-mode)
+(global-set-key (kbd "<escape>") 'god-local-mode)
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'box
+                      'bar)))
+
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
+(define-key god-local-mode-map (kbd ".") 'repeat)
+(define-key god-local-mode-map (kbd "i") 'god-local-mode)
+(global-set-key (kbd "C-x C-1") 'delete-other-windows)
+(global-set-key (kbd "C-x C-2") 'split-window-below)
+(global-set-key (kbd "C-x C-3") 'split-window-right)
+(global-set-key (kbd "C-x C-0") 'delete-window)
+(global-set-key (kbd "C-x C-o") 'other-window)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ido
